407c188ce2191b8ba87a903ea2d42bd80e133efd
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-11-04 10:21:36 -0600

    cambios en enteregar

--- resources/views/entregar/detalle.blade.php
+++ resources/views/entregar/detalle.blade.php
@@ -95,7 +95,7 @@ License: For each use you must have a valid license purchased only from above li
                                         <h3 class="card-title align-items-start flex-column">
                                             <span class="card-label fw-bold fs-3 mb-1 text-gray-600 text-uppercase">
                                                
-                                                ENVIOS POR ENTREGAR
+                                                ENVIOS POR ENTREGARR
                                                
                                             </span>
                                             <span class="text-muted mt-1 fw-semibold fs-7">
