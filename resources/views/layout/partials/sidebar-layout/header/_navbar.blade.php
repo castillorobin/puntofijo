d60565cf94aec9dfadaa3ded216c5a2b3af883d3
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-11-07 21:36:56 -0600

    cambios en menu header

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -11,13 +11,13 @@
 		<div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
 			<img src="{{ image('avatars/300-1.jpg') }}" alt="user" />
 		</div>
-		@include('partials/menus/_user-account-menu')
+	</div>
 		<!--end::Menu wrapper-->
 	</div>
 	<!--end::User menu-->
 	<!--begin::Header menu toggle-->
 	<div class="app-navbar-item d-lg-none ms-2 me-n3" title="Show header menu">
-		<div class="btn btn-icon btn-active-color-primary w-30px h-30px w-md-35px h-md-35px" id="kt_app_header_menu_toggle">{!! getIcon('text-align-left', 'fs-2 fs-md-1') !!}</div>
+		
 	</div>
 	<!--end::Header menu toggle-->
 </div>
