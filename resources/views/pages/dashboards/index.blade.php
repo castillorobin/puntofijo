a9a588634d1dbe086f33bd4d6fd0fb57ebd81fbc
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-11-15 11:34:51 -0600

    cambios en logo

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -80,7 +80,7 @@ License: For each use you must have a valid license purchased only from above li
 				<!--begin::Logo-->
 				<div class="aside-logo flex-column-auto pt-10 pt-lg-20" id="kt_aside_logo">
 					<a href="/dashboard">
-						<img alt="Logo" src="assets/media/logos/demo9.svg" class="h-40px" />
+						<img alt="Logo" src="assets/media/logos/logo.png" class="h-40px" />
 					</a>
 				</div>
 				<!--end::Logo-->
